f9bacd1afea9369c05665afa50416c7e9db0f6f9
chriscodehead <chrischikabox@gmail.com>
2023-08-18 18:01:19 +0100

    Newer push

---
+++ head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="utf-8" />
+ <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
+ <meta http-equiv="x-ua-compatible" content="ie=edge" />
+ <title>Home V.2 – Collab – Online Learning Platform Template</title>
+ <link rel="shortcut icon" href="assets/images/logo/favourite_icon_1.svg" />
+ <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
